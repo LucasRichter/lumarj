5eb82b740dc6724afc6d79ca935806eb1105cd6d
Lucas Richter <richterlucas@hotmail.com>
2017-09-03 18:33:59 -0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var concat = require( 'gulp-concat' )
+var rename = require( 'gulp-rename' )
+var uglify = require( 'gulp-uglify' )
+var pug = require( 'gulp-pug' )
+var sass = require( 'gulp-sass' )
+var image = require( 'gulp-image' )
+var gulp = require( 'gulp' )
+var child = require( 'child_process' )
+var fs = require( 'fs' )
+var clean = require( 'gulp-clean' )
+
+const cleanPath = ( path ) => {
+  return gulp.src( path, { read: false } )
+  .pipe( clean({ force: true }) )
+}
+
+gulp.task( 'clean-pug', () => cleanPath( './dest/*.html' ) )
+
+gulp.task( 'js', () => {
+  return gulp.src( './src/js/**/*.js' )
+    .pipe( gulp.dest( 'dest/js' ) )
+} )
+
+gulp.task( 'views', [ 'clean-pug' ], () => {
+  return gulp.src( './src/views/*.pug' )
+  .pipe( pug() )
+  .pipe( gulp.dest( 'dest' ) )
+} )
+
+gulp.task( 'sass', () => {
+  return gulp.src( './src/css/main.sass' )
+    .pipe( sass().on('error', sass.logError) )
+    .pipe( gulp.dest('./dest/css') )
+} )
+
+gulp.task( 'image', () => {
+  return gulp.src('./src/image/**/*')
+    .pipe( gulp.dest( './dest/image' ) )
+} )
+
+gulp.task( 'fonts', () => {
+  return gulp.src( './src/fonts/*' )
+  .pipe( gulp.dest( './dest/fonts/' ) )
+} )
+
+gulp.task( 'watch', () => {
+  gulp.watch( './src/css/**/*.sass', ['sass'] )
+  gulp.watch( './src/js/**/*.js', ['js'] )
+  gulp.watch( './src/image/**/*', ['image'] )
+  gulp.watch( './src/views/**/*.pug', [ 'views' ] )
+} )
+
+gulp.task('connect', () => {
+  var server = child.spawn( 'node', ['server.js'] )
+  var log = fs.createWriteStream( 'server.log', {flags: 'a'} )
+  server.stdout.pipe( log )
+  server.stderr.pipe( log )
+} )
+
+gulp.task( 'default', [ 'connect', 'js', 'sass', 'views', 'image', 'watch', 'fonts' ] )
---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+new Swiper( '.swiper-container', {
+	nextButton: '#next',
+	prevButton: '#prev',
+	spaceBetween: 50,
+	slidesPerView: 3,
+	autoHeight: true,
+	setWrapperSize: true,
+
+	breakpoints: {
+		767: {
+			slidesPerView: 1,
+			spaceBetween: 30
+		}
+	}
+} )
+
+new Swiper( '#home-swiper-f', {
+	parallax: true,
+	autoHeight: true,
+	autoplay: 2000,
+	loop: true,
+	setWrapperSize: true,
+	effect: 'fade',
+} )
+
+new Swiper( '#home-swiper-s', {
+	nextButton: '#nextS',
+	prevButton: '#prevS',
+	autoHeight: true,
+	slidesPerView: 3,
+	pagination: '.swiper-pagination',
+
+	breakpoints: {
+		767: {
+			slidesPerView: 1,
+			spaceBetween: 30
+		}
+	}
+} )
